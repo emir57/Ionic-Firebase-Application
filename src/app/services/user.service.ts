efdbde9e11798960eccd41ce3d1b7a711809d4c6
emir57 <emir.gurbuz06@hotmail.com>
2021-12-17 19:30:53 +0300

    UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private firebaseStore:AngularFirestore
+  ) { }
+
+  addUser(user:User){
+    return this.firebaseStore.collection("users").add(user);
+  }
+  updateUser(user:User){
+    return this.firebaseStore.collection("users").doc(user.id).update(user);
+  }
+  deleteUser(id:string){
+    return this.firebaseStore.collection("users").doc(id).delete();
+  }
 }
