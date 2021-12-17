950e98c2018411a1c595f4dc83ef5c159e065796
emir57 <emir.gurbuz06@hotmail.com>
2021-12-17 17:41:36 +0300

    Complete RoleGuard

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { User } from '../models/user';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const subject = new Subject<boolean>();
+      this.authService.getCurrentUser().subscribe(user => {
+        if(this.isInRole(user,"Admin")){
+          subject.next(true);
+        }else{
+          subject.next(false);
+        }
+      })
+    return subject.asObservable();
   }
-  
+
+  isInRole(user:User,role:string) {
+    let check = false;
+    let roles= user.roles.split(",");
+    roles.forEach(r=>{
+      if(r.toLocaleLowerCase()===role.toLocaleLowerCase()){
+        check=true;
+      }
+    })
+    return check;
+  }
+
 }
