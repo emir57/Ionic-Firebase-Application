5bd6b3ed5b313cf6344c78694638d24fe8c0d76c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-18 18:15:18 +0300

    Payment

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { CartModel } from '../models/cartModel';
 import { Product } from '../models/product';
 import { User } from '../models/user';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-payment',
@@ -11,56 +12,70 @@ import { User } from '../models/user';
   styleUrls: ['./payment.page.scss'],
 })
 export class PaymentPage implements OnInit {
-  @Input() carts:CartModel[]
-  @Input() user:User
+  @Input() carts: CartModel[]
+  @Input() user: User
 
-  totalPrice=0;
-  form:FormGroup
+  totalPrice = 0;
+  form: FormGroup
   constructor(
-    private modalController:ModalController,
-    private formBuilder:FormBuilder
+    private modalController: ModalController,
+    private formBuilder: FormBuilder,
+    private orderService: OrderService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
-    this.carts.forEach(c=>{
-      if(c.product.isDiscount){
-        this.totalPrice += (c.product.unitPrice - (c.product.unitPrice*c.product.discount/100))*c.quantity
-      }else{
-        this.totalPrice += (c.product.unitPrice)*c.quantity
+    this.carts.forEach(c => {
+      if (c.product.isDiscount) {
+        this.totalPrice += (c.product.unitPrice - (c.product.unitPrice * c.product.discount / 100)) * c.quantity
+      } else {
+        this.totalPrice += (c.product.unitPrice) * c.quantity
       }
     })
     this.createForm();
     console.log(this.totalPrice)
   }
 
-  createForm(){
+  createForm() {
     this.form = this.formBuilder.group({
-      creditCardNumber:['1111 1111 1111 1111',[Validators.required,Validators.maxLength(19)]],
-      cvv:['994',[Validators.required,Validators.maxLength(3)]],
-      date:['02/26',[Validators.required]],
-      city:['Ankara',[Validators.required]],
-      addressText:['Örnek Mahallesi\nFaik Suat Caddesi\nAnkara/Altındağ',[Validators.required]],
-      totalPrice:[this.totalPrice]
+      creditCardNumber: ['1111 1111 1111 1111', [Validators.required, Validators.maxLength(19)]],
+      cvv: ['994', [Validators.required, Validators.maxLength(3)]],
+      date: ['02/26', [Validators.required]],
+      city: ['Ankara', [Validators.required]],
+      addressText: ['Örnek Mahallesi\nFaik Suat Caddesi\nAnkara/Altındağ', [Validators.required]],
+      totalPrice: [this.totalPrice]
     })
   }
-  payment(){
-    if(this.form.valid){
-      let products:Product[]=[]
-      this.carts.forEach(c=>{
-        products.push(Object.assign({quantity:c.quantity},c.product))
+  payment() {
+    if (this.form.valid) {
+      let products: Product[] = []
+      this.carts.forEach(c => {
+        products.push(Object.assign({ quantity: c.quantity }, c.product))
       })
       let order = Object.assign(
         {
-          userId:this.user.id,
-          products:products
+          userId: this.user.id,
+          products: products
         },
         this.form.value)
-      console.log(order)
+      setTimeout(() => {
+        this.orderService.addOrder(order).finally(() => {
+          this.presentToast("Ödeme Başarılı")
+        })
+      }, 5000);
     }
   }
 
-  dismiss(){
+  dismiss() {
     this.modalController.dismiss();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
