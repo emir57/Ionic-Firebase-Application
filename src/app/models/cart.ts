af3a6700a72c40d8b32d00df16a36c44231870f1
emir57 <emir.gurbuz06@hotmail.com>
2021-12-18 15:04:18 +0300

    Cart Service

--- src/app/models/cart.ts
+++ src/app/models/cart.ts
@@ -2,6 +2,7 @@ import { Product } from "./product";
 import { User } from "./user";
 
 export interface Cart{
+  id?:string;
   product:Product,
   user:User,
   quantity:number;
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
+import { Cart } from '../models/cart';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,41 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class CartService {
 
   constructor(
-    private fireStore:AngularFirestore
+    private fireStore: AngularFirestore
   ) { }
+
+  addToCart(cart: Cart) {
+    const carts = this.fireStore.collection("carts");
+    return carts.add(cart);
+  }
+
+  deleteCart(id: string) {
+    const carts = this.fireStore.collection("carts")
+    this.getCart(id).subscribe(cart => {
+      if (cart.quantity > 1) {
+        cart.quantity -= 1
+        carts.doc(id).update(cart);
+      } else {
+        carts.doc(id).delete();
+      }
+    })
+  }
+  getCart(id: string): Observable<any> {
+    const subject = new Subject<any>();
+    const carts = this.fireStore.collection("carts")
+    carts.doc(id).get().subscribe(doc => {
+      subject.next(Object.assign({ id: doc.id }, doc.data()))
+    })
+    return subject.asObservable();
+  }
+  getCarts() {
+    let carts:any[]=[];
+    const subject = new Subject<any>();
+    const cart = this.fireStore.collection("carts")
+    cart.get().subscribe(doc => {
+      doc.forEach(d=>carts.push(Object.assign({id:d.id},d.data())))
+      subject.next(carts)
+    })
+    return subject.asObservable();
+  }
 }
